ff6ebd2fc9252836b1cc087d14508c814bbd4b6c
francesco-vaselli <francesco.vaselli@protonmail.com>
2021-10-18 16:22:24 +0200

    barely working

--- order0/data_gen.py
+++ order0/data_gen.py
@@ -6,16 +6,17 @@ if __name__ == '__main__':
 
     rng = default_rng(42)
 
-    dataset_size = 1e6
+    dataset_size = 5e6
     dataset = []
 
     for i in range(0, int(dataset_size)):
 
-        gen = rng.integers(1, 11)
+        gen = rng.integers(5, 11)
+        # gen = 10
         mu, sigma = gen, gen*0.1 # mean and standard deviation
         reco = rng.normal(mu, sigma)
 
         dataset.append([gen, reco])
     
     dataset = np.array(dataset, dtype=np.float32)
-    np.save('dataset.npy', dataset)
+    np.save('order0/dataset.npy', dataset)
