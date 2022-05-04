64b230ff4f1e67291be750274b2185487308a693
francesco-vaselli <71897901+francesco-vaselli@users.noreply.github.com>
2022-05-04 09:49:15 +0200

    updated full muon processing

--- order2/h5py_muonData.py
+++ order2/h5py_muonData.py
@@ -5,7 +5,7 @@ import numpy as np
 
 
 if __name__=='__main__':
-	f = 7 
+	f = 1 
 	tree = uproot.open(f"MMuons{f}.root:MMuons", num_workers=20)
 	vars_to_save = tree.keys()
 	print(vars_to_save)
@@ -15,7 +15,7 @@ if __name__=='__main__':
 	print(df)
 	
 	df = df.drop(["MGenPart_statusFlags2", "MGenPart_statusFlags12", "MGenPart_statusFlags14"], axis=1)
-	dr = df[~df.isin([np.nan, np.inf, -np.inf]).any(1)]	
+	df = df[~df.isin([np.nan, np.inf, -np.inf]).any(1)]	
 	# apply cuts
 	df = df[df.MMuon_dxy < 1.5]
 	df = df[df.MMuon_dxy > -1.5]
@@ -31,26 +31,51 @@ if __name__=='__main__':
 	df = df[df.MMuon_pfRelIso04_all < 70]
 	df = df[df.MMuon_ptErr < 20]
 	df = df[df.MMuon_sip3d < 1000]
-	df['MMuon_etaMinusGen'] = df['MMuon_etaMinusGen'].apply(lambda x: x*100)
-	df['MMuon_phiMinusGen'] = df['MMuon_phiMinusGen'].apply(lambda x: x*100)
-	df['MMuon_dxy'] = df['MMuon_dxy'].apply(lambda x: x*10)
-	df['MMuon_dxyErr'] = df['MMuon_dxyErr'].apply(lambda x: np.log(x)+6.5)
-	df['MMuon_dz'] = df['MMuon_dz'].apply(lambda x: x*10)
-	df['MMuon_dzErr'] = df['MMuon_dzErr'].apply(lambda x: np.log(x)+5.5)
-	df['MMuon_ip3d'] = df['MMuon_ip3d'].apply(lambda x: np.log(x)+5)
-	df['MMuon_jetPtRelv2'] = df['MMuon_jetPtRelv2'].apply(lambda x: np.log1p(x))
-	df['MMuon_jetRelIso'] = df['MMuon_jetRelIso'].apply(lambda x: np.log1p(x))
-	df['MMuon_pfRelIso03_all'] = df['MMuon_pfRelIso03_all'].apply(lambda x: np.log1p(x))
-	df['MMuon_pfRelIso03_chg'] = df['MMuon_pfRelIso03_chg'].apply(lambda x: np.log1p(x))
-	df['MMuon_pfRelIso04_all'] = df['MMuon_pfRelIso04_all'].apply(lambda x: np.log1p(x))
-	df['MMuon_ptErr'] = df['MMuon_ptErr'].apply(lambda x: np.log(x)+1.5)
-	df['MMuon_sip3d'] = df['MMuon_sip3d'].apply(lambda x: np.log(x))
+	#  what if we trained without transforms?
+	df["MGenMuon_pt"] = df["MGenMuon_pt"].apply(lambda x: np.log(x))
+	df["MClosestJet_pt"] = df["MClosestJet_pt"].apply(lambda x: np.log1p(x))
+	df["MClosestJet_mass"] = df["MClosestJet_mass"].apply(lambda x: np.log1p(x))
+	df["Pileup_sumEOOT"] = df["Pileup_sumEOOT"].apply(lambda x: np.log(x))
+	df["Pileup_sumLOOT"] = df["Pileup_sumLOOT"].apply(lambda x: np.log1p(x))
+	df["MMuon_etaMinusGen"] = df["MMuon_etaMinusGen"].apply(lambda x: np.arctan(x*100))
+	df["MMuon_phiMinusGen"] = df["MMuon_phiMinusGen"].apply(lambda x: np.arctan(x*80))
+	df["MMuon_ptRatio"] = df["MMuon_ptRatio"].apply(lambda x: np.arctan((x-1)*10))
+	df["MMuon_dxy"] = df["MMuon_dxy"].apply(lambda x: np.arctan(x*150))
+	df["MMuon_dxyErr"] = df["MMuon_dxyErr"].apply(lambda x: np.log1p(x))
+	df["MMuon_dz"] = df["MMuon_dz"].apply(lambda x: np.arctan(x*50))
+	df["MMuon_dzErr"] = df["MMuon_dzErr"].apply(lambda x: np.log(x+0.001))
+	df["MMuon_ip3d"] = df["MMuon_ip3d"].apply(lambda x: np.log(x+0.001))
+	df["MMuon_jetPtRelv2"] = df["MMuon_jetPtRelv2"].apply(lambda x: np.log(x+0.001))
+	arr = df["MMuon_jetPtRelv2"].values
+	arr[arr<=-4] = np.random.normal(loc=-6.9, scale=1, size=arr[arr<=-4].shape)
+	df["MMuon_jetPtRelv2"] = arr
+	df["MMuon_jetRelIso"] = df["MMuon_jetRelIso"].apply(lambda x: np.log(x+0.08))
+	df["MMuon_pfRelIso04_all"] = df["MMuon_pfRelIso04_all"].apply(lambda x: np.log(x+0.00001))
+	arr = df["MMuon_pfRelIso04_all"].values
+	arr[arr<=-7.5] = np.random.normal(loc=-11.51, scale=1, size=arr[arr<=-7.5].shape)
+	df["MMuon_pfRelIso04_all"] = arr
+	df["MMuon_pfRelIso03_all"] = df["MMuon_pfRelIso03_all"].apply(lambda x: np.log(x+0.00001))
+	arr = df["MMuon_pfRelIso03_all"].values
+	arr[arr<=-7.5] = np.random.normal(loc=-11.51, scale=1, size=arr[arr<=-7.5].shape)
+	df["MMuon_pfRelIso03_all"] = arr
+	df["MMuon_pfRelIso03_chg"] = df["MMuon_pfRelIso03_chg"].apply(lambda x: np.log(x+0.00001))
+	arr = df["MMuon_pfRelIso03_chg"].values
+	arr[arr<=-7.5] = np.random.normal(loc=-11.51, scale=1, size=arr[arr<=-7.5].shape)
+	df["MMuon_pfRelIso03_chg"] = arr
+	df["MMuon_ptErr"] = df["MMuon_ptErr"].apply(lambda x: np.log(x+0.001))
+	df["MMuon_sip3d"] = df["MMuon_sip3d"].apply(lambda x: np.log1p(x))
+	df['MMuon_isGlobal'] = df['MMuon_isGlobal'].apply(lambda x: x + 0.1*np.random.normal())
+	df['MMuon_isPFcand'] = df['MMuon_isPFcand'].apply(lambda x: x + 0.1*np.random.normal())
+	df['MMuon_isTracker'] = df['MMuon_isTracker'].apply(lambda x: x + 0.1*np.random.normal())
+	df['MMuon_mediumId'] = df['MMuon_mediumId'].apply(lambda x: x + 0.1*np.random.normal())
+	df['MMuon_softId'] = df['MMuon_softId'].apply(lambda x: x + 0.1*np.random.normal())
+	df['MMuon_softMvaId'] = df['MMuon_softMvaId'].apply(lambda x: x + 0.1*np.random.normal())
+	df = df[~df.isin([np.nan, np.inf, -np.inf]).any(1)]
 	print(df)
 		
 	# open hdf5 file for saving
-	f = h5py.File(f'muons{f}.hdf5','w')
+	f = h5py.File(f'tmuons{f}.hdf5','w')
 
-	dset = f.create_dataset("data", data=df.values)#, dtype='f4')
+	dset = f.create_dataset("data", data=df.values, dtype='f4')
 
 	f.close()
-	
